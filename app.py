d95352d4146af4b48e0520e22eeca1f5eaea4f3e
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-03-21 23:15:47 +0100

    feat() Include other endpoints
    All other endpoints for the store
    
    [S3,L43]

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -22,12 +22,21 @@ def home():
 
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store), 201
 
 
-@app.route('/store/<string:name>', methods=['GET'])
+@app.route('/store/<string:store_name>', methods=['GET'])
 def get_store(store_name):
-    pass
+    store = [store for store in stores if store['name'] == store_name]
+    if store:
+        return jsonify(store)
+    return jsonify({}), 404
 
 
 @app.route('/store', methods=['GET'])
@@ -35,14 +44,25 @@ def get_stores():
     return jsonify({'stores': stores})
 
 
-@app.route('/store/<string:name>/item', methods=['POST'])
+@app.route('/store/<string:store_name>/item', methods=['POST'])
 def create_item(store_name):
-    pass
+    request_data = request.get_json()
+    store = [store for store in stores if store['name'] == store_name]
+    if store:
+        store[0]['items'].append(request_data)
+        return jsonify(request_data), 201
+    return jsonify({}), 404
 
 
-@app.route('/store/<string:name>/item', methods=['GET'])
-def get_item_in_store(store_name):
-    pass
+@app.route('/store/<string:store_name>/item/<string:item_name>', methods=['GET'])
+def get_item_in_store(store_name, item_name):
+    store = [store for store in stores if store['name'] == store_name]
+    if store:
+        store = store[0]
+        item = [item for item in store['items'] if item['name'] == item_name]
+        if item:
+            return jsonify(item)
+    return jsonify({}), 404
 
 
 app.run(port=5000, debug=True)
